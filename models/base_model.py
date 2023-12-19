077000eb4dea5985c7795ddcc9c8a14f73d85952
bakiizese <bereketzeselassie@gmail.com>
2023-12-19 12:07:36 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,21 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import  declarative_base
 
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 class BaseModel:
-
-    """id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DataTime, nullable=False, default=datetime.utcnow())
-    A base class for all hbnb models"""
+    if models.storage_t == "db":
+        id = Column(String(60), unique=True, nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
 
@@ -22,7 +29,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -33,6 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        models.storage.new(self)
         storage.new(self)
         storage.save()
 
@@ -45,3 +52,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    
+    def delete(self):
+        """delete current"""
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Foreignkey
 
+class City(BaseModel, Base):
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(128), nullable=False, ForeignKey('states_id')
+    else:
+        state_id = ""
+        name = ""
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
