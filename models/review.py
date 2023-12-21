dce7fa1ee66c7353d5d18d1c265893e6830fd736
bakiizese <bereketzeselassie@gmail.com>
2023-12-20 16:23:22 -0800

    fix

--- models/review.py
+++ models/review.py
@@ -2,11 +2,13 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
+import models
+
 
 class Review(BaseModel):
     """ Review classto store review information """
     if models.storage == 'db':
-        __tablename = 'reviews'
+        __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
