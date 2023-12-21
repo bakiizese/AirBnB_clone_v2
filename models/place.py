9c4d283670af52958b3fae997c291ffb9fceb8ac
bakiizese <bereketzeselassie@gmail.com>
2023-12-20 16:29:51 -0800

    fix

--- models/place.py
+++ models/place.py
@@ -7,6 +7,18 @@ import sqlalchemy
 from os import getenv
 import models
 
+
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
+
 class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
@@ -21,7 +33,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place')
+        reviews = relationship("Review", backref="place")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities",
                                  viewonly=False)
