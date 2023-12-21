34ff2562692b89d0f03d1fee091b4515251eabea
bakiizese <bereketzeselassie@gmail.com>
2023-12-20 16:25:26 -0800

    fix

--- models/review.py
+++ models/review.py
@@ -5,9 +5,9 @@ from sqlalchemy import Column, String, ForeignKey
 import models
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    if models.storage == 'db':
+    if models.storage_t == 'db':
         __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
