45a3bcdc459f72aed5de73adf3dfd739be971ac8
bakiizese <bereketzeselassie@gmail.com>
2024-01-11 07:51:23 -0800

    fix

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''fabric;'''
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """do packing"""
+
+
+    tm = datetime.now()
+    arch = 'web_static_' + tm.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    lc = local('tar -cvzf versions/{} web_static'.format(arch))
+    if lc is not None:
+        return arch
+    else:
+        return None
