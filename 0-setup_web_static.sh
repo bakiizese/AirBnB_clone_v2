f8b08662cec2ac94de3a64d94fb9fcf49fbc3b57
bakiizese <bereketzeselassie@gmail.com>
2024-01-11 04:34:07 -0800

    sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+!/usr/bin/env bash
+#installing nginx and createing dir
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "hello alx" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
