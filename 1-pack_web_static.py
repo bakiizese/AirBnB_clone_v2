42e4270322b91c62b94962775b43ddb401ef109a
bakiizese <bereketzeselassie@gmail.com>
2024-01-11 07:52:46 -0800

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,6 @@ from fabric.api import *
 def do_pack():
     """do packing"""
 
-
     tm = datetime.now()
     arch = 'web_static_' + tm.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
