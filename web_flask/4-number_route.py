ce0fbabdb0de6f05897225be54026835c2af4bee
bakiizese <bereketzeselassie@gmail.com>
2024-01-26 03:01:10 -0800

    fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -39,7 +39,10 @@ def number(n):
         int(n)
         return "{} is a number".format(n)
     except ValueError:
-        pass
+       return '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<title>404 Not Found</title>
+<h1>Not Found</h1>
+<p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>\n'''
 
 
 if __name__ == "__main__":
